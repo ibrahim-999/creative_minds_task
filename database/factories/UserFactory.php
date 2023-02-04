4f85ad9c4f90a30454ea335c744466cbba2e46c1
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-02-04 15:03:08 +0200

    [Add] fake data for testing

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,13 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'username' => fake()->name(),
+            'phone' => fake()->phoneNumber,
+            'phone_verified_at' => now(),
+            'verification_code' => fake()->countryCode,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'type' => 'user'
         ];
     }
 
