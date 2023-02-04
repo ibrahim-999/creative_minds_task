fb63156891cbe89052f7c47a8ac29e8173691231
ibrahim-999 <ibrahimkhalaf99@gmail.com>
2023-02-04 14:20:16 +0200

    [ADD] laravel telescope

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('mobile')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('type');
             $table->rememberToken();
             $table->timestamps();
         });
